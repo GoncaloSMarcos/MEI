8d66c1bc1e5b4710e5499a2d6cf53be9e935517d
Josphze <josphze@gmail.com>
2021-10-31 17:42:15 +0000

    Test prob feito

--- script.sh
+++ script.sh
@@ -4,18 +4,18 @@ gcc code1.c -o code1 -O3
 gcc code2.c -o code2 -O3
 
 
-MAX_ITERATIONS=19
+MAX_ITERATIONS=2
 
 FILENAME_DATA_INPUT="data.in"
 
 # Variáveis do gen.py
 N_EXAMS=20    # Max 299 segundo o enunciado
-PROBABILITY_PAIRS=5 # 0.0 a 1.0
-PROBABILTY_PAIRS_INCREMENT_VALUE=5
+PROBABILITY_PAIRS=96 # 0.0 a 1.0
+PROBABILTY_PAIRS_INCREMENT_VALUE=2
 SEED_GEN=$(shuf -i1-30000 -n1) 
 # Variáveis do programa
 SEED_PROGRAM=$(shuf -i1-30000 -n1) 
-MAX_TIME=60  #in seconds
+MAX_TIME=120  #in seconds
 
 
 
@@ -28,7 +28,7 @@ while [ $I -le $MAX_ITERATIONS ]
 do
     
 
-    for j in 1 2 3 4 5 6 7 8 9 10
+    for j in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
     do
         # Run code
         python3 gen.py $N_EXAMS $PROBABILITY_PAIRS $SEED_GEN $FILENAME_DATA_INPUT
