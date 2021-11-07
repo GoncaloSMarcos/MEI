9ff4dcc2b372a67570452eb6cf180b70fd419519
Gonçalo Filipe Fernandes São Marcos <gmarcos@student.dei.uc.pt>
2021-11-07 15:15:45 +0000

    novos testes

--- r.r
+++ r.r
@@ -1,23 +1,190 @@
-data1 = read.table("results_code1.txt", header=TRUE)
-data2 = read.table("results_code2.txt", header=TRUE)
+# ------------------ TESTES PROBABILIDADE ------------------ 
+testeProb1 = read.table("testeProb1.txt", header=TRUE)
+
+n <- boxplot(time_in_seconds ~ probability_pairs, data = testeProb1);
+testeProb1_without_outlier <- testeProb1[-which(testeProb1$time_in_seconds %in% n$out), ]
+ag_testeProb1 = aggregate(time_in_seconds ~ probability_pairs, data = testeProb1_without_outlier, mean)
+
+plot(testeProb1$probability_pairs,testeProb1$time_in_seconds)
+
+#-----
+testeProb2 = read.table("testeProb2.txt", header=TRUE)
+
+n <- boxplot(time_in_seconds ~ probability_pairs, data = testeProb2);
+testeProb2_without_outlier <- testeProb2[-which(testeProb2$time_in_seconds %in% n$out), ]
+ag_testeProb2 = aggregate(time_in_seconds ~ probability_pairs, data = testeProb2_without_outlier, mean)
+
+plot(testeProb2$probability_pairs,testeProb2$time_in_seconds)
 
 
-n <- boxplot(time_in_seconds ~ probability_pairs, data = data1);
+# ------------------ TESTES EXAMES ------------------ 
+testeExams1 = read.table("testeExams1.txt", header=TRUE)
 
-data1_without_outlier <- data1[-which(data1$time_in_seconds %in% n$out), ]
+n <- boxplot(time_in_seconds ~ n_exams, data = testeExams1);
+testeExams1_without_outlier <- testeExams1[-which(testeExams1$time_in_seconds %in% n$out), ]
+ag_testeExams1 = aggregate(time_in_seconds ~ n_exams, data = testeExams1_without_outlier, mean)
 
-ag = aggregate(time_in_seconds ~ probability_pairs, data = data1_without_outlier, mean)
+plot(testeExams1$n_exams,testeExams1$time_in_seconds, 
+     main = "Teste Exames 1 - Raw data", 
+     xlab = "Número de Exames",
+     ylab ="Tempo de execução (em segundos)")
+plot(testeExams1_without_outlier$n_exams,testeExams1_without_outlier$time_in_seconds, 
+     main = "Teste Exames 1 - Sem outliers", 
+     xlab = "Número de Exames",
+     ylab ="Tempo de execução (em segundos)")
+plot(ag_testeExams1$n_exams,ag_testeExams1$time_in_seconds, 
+     main = "Teste Exames 1 - Sem outliers, valor médio por número de exames", 
+     xlab = "Número de Exames",
+     ylab ="Tempo de execução (em segundos)")
 
-plot(ag$probability_pairs,ag$time_in_seconds)
+ag_testeExames1_com_outliers = aggregate(time_in_seconds ~ n_exams, data = testeExams1, mean)
+
+plot(ag_testeExames1_com_outliers$n_exams,ag_testeExames1_com_outliers$time_in_seconds, 
+     main = "Teste Exames 1 - Com outliers, valor médio por número de exames", 
+     xlab = "Número de Exames",
+     ylab ="Tempo de execução (em segundos)",
+     xlim = c(0,60),
+     ylim = c(0,60))
+
+
+#-----
+testeExams2 = read.table("testeExams2.txt", header=TRUE)
+
+n <- boxplot(time_in_seconds ~ n_exams, data = testeExams2);
+testeExams2_without_outlier <- testeExams2[-which(testeExams2$time_in_seconds %in% n$out), ]
+ag_testeExams2 = aggregate(time_in_seconds ~ n_exams, data = testeExams2_without_outlier, mean)
+
+plot(testeExams2$n_exams,testeExams2$time_in_seconds, 
+     main = "Teste Exames 2 - Raw data", 
+     xlab = "Número de Exames",
+     ylab ="Tempo de execução (em segundos)")
+plot(testeExams2_without_outlier$n_exams,testeExams2_without_outlier$time_in_seconds, 
+     main = "Teste Exames 2 - Sem outliers", 
+     xlab = "Número de Exames",
+     ylab ="Tempo de execução (em segundos)")
+plot(ag_testeExams2$n_exams,ag_testeExams2$time_in_seconds, 
+     main = "Teste Exames 2 - Sem outliers, valor médio por número de exames", 
+     xlab = "Número de Exames",
+     ylab ="Tempo de execução (em segundos)")
+
+
+
+
+# ------------------ TESTES EXAMES 35 60 ------------------ 
+testeExames1_35_60 = read.table("testeExames1_35-60.txt", header=TRUE)
+
+n <- boxplot(time_in_seconds ~ n_exams, data = testeExames1_35_60);
+testeExames1_35_60_without_outlier <- testeExames1_35_60[-which(testeExames1_35_60$time_in_seconds %in% n$out), ]
+ag_testeExames1_35_60 = aggregate(time_in_seconds ~ n_exams, data = testeExames1_35_60_without_outlier, mean)
+
+plot(testeExames1_35_60$n_exams,testeExames1_35_60$time_in_seconds, 
+     main = "Teste Exames 1 (35 a 60) - Raw data", 
+     xlab = "Número de Exames",
+     ylab ="Tempo de execução (em segundos)")
+plot(testeExames1_35_60_without_outlier$n_exams,testeExames1_35_60_without_outlier$time_in_seconds, 
+     main = "Teste Exames 1 (35 a 60) - Sem outliers", 
+     xlab = "Número de Exames",
+     ylab ="Tempo de execução (em segundos)")
+plot(ag_testeExames1_35_60$n_exams,ag_testeExames1_35_60$time_in_seconds, 
+     main = "Teste Exames 1 (35 a 60) - Sem outliers, valor médio por número de exames", 
+     xlab = "Número de Exames",
+     ylab ="Tempo de execução (em segundos)")
+
+
+
+ag_testeExames1_35_60_com_outliers = aggregate(time_in_seconds ~ n_exams, data = testeExames1_35_60, mean)
+
+plot(ag_testeExames1_35_60_com_outliers$n_exams,ag_testeExames1_35_60_com_outliers$time_in_seconds, 
+     main = "Teste Exames 1 (35 a 60) - Com outliers, valor médio por número de exames", 
+     xlab = "Número de Exames",
+     ylab ="Tempo de execução (em segundos)")
+
+
+
+#-----
+
+testeExames2_35_60 = read.table("testeExames2_35-60.txt", header=TRUE)
+
+n <- boxplot(time_in_seconds ~ n_exams, data = testeExames2_35_60);
+testeExames2_35_60_without_outlier <- testeExames2_35_60[-which(testeExames2_35_60$time_in_seconds %in% n$out), ]
+ag_testeExames2_35_60 = aggregate(time_in_seconds ~ n_exams, data = testeExames2_35_60_without_outlier, mean)
+
+plot(testeExames2_35_60$n_exams,testeExames2_35_60$time_in_seconds, 
+     main = "Teste Exames 2 (35 a 60) - Raw data", 
+     xlab = "Número de Exames",
+     ylab ="Tempo de execução (em segundos)")
+plot(testeExames2_35_60_without_outlier$n_exams,testeExames2_35_60_without_outlier$time_in_seconds, 
+     main = "Teste Exames 2 (35 a 60) - Sem outliers", 
+     xlab = "Número de Exames",
+     ylab ="Tempo de execução (em segundos)")
+plot(ag_testeExames2_35_60$n_exams,ag_testeExames2_35_60$time_in_seconds, 
+     main = "Teste Exames 2 (35 a 60) - Sem outliers, valor médio por número de exames", 
+     xlab = "Número de Exames",
+     ylab ="Tempo de execução (em segundos)")
+
+
+ag_testeExames2_35_60_com_outliers = aggregate(time_in_seconds ~ n_exams, data = testeExames2_35_60, mean)
+
+plot(ag_testeExames2_35_60_com_outliers$n_exams,ag_testeExames2_35_60_com_outliers$time_in_seconds, 
+     main = "Teste Exames 2 (35 a 60) - Com outliers, valor médio por número de exames", 
+     xlab = "Número de Exames",
+     ylab ="Tempo de execução (em segundos)")
+
+
+
+
+
+# ------------------ TESTES EXAME E PROB ------------------ 
+
+testeExames_Prob1 = read.table("teste_Exames_Prob1.txt", header=TRUE)
+
+n <- boxplot(time_in_seconds ~ n_exams, data = testeExames_Prob1);
+testeExames_Prob1_without_outlier <- testeExames_Prob1[-which(testeExames_Prob1$time_in_seconds %in% n$out), ]
+ag_testeExames_Prob1 = aggregate(time_in_seconds ~ n_exams, data = testeExames_Prob1_without_outlier, mean)
+
+plot(testeExames_Prob1$n_exams,testeExames_Prob1$time_in_seconds, 
+     main = "Teste Exames e Prob 1 - Raw data (Probabilidades 2,5,25,50)", 
+     xlab = "Número de Exames",
+     ylab ="Tempo de execução (em segundos)")
+plot(testeExames_Prob1_without_outlier$n_exams,testeExames_Prob1_without_outlier$time_in_seconds, 
+     main = "Teste Exames e Prob 1 - Sem outliers (Probabilidades 2,5,25,50)", 
+     xlab = "Número de Exames",
+     ylab ="Tempo de execução (em segundos)")
+plot(ag_testeExames_Prob1$n_exams,ag_testeExames_Prob1$time_in_seconds, 
+     main = "Teste Exames e Prob 1 - Sem outliers, valor médio por número de exames (Probabilidades 2,5,25,50)", 
+     xlab = "Número de Exames",
+     ylab ="Tempo de execução (em segundos)")
+
+# ------------------  ------------------ 
+
+testeExames_Prob1 = read.table("teste_Exames_Prob1.txt", header=TRUE)
+
+n <- boxplot(time_in_seconds ~ probability_pairs, data = testeExames_Prob1);
+testeExames_Prob1_without_outlier <- testeExames_Prob1[-which(testeExames_Prob1$time_in_seconds %in% n$out), ]
+ag_testeExames_Prob1 = aggregate(time_in_seconds ~ probability_pairs, data = testeExames_Prob1_without_outlier, mean)
+
+plot(testeExames_Prob1$probability_pairs,testeExames_Prob1$time_in_seconds, 
+     main = "Teste Exames e Prob 1 - Raw data (Exames 10 a 60, 2 em 2)", 
+     xlab = "Probabilidade",
+     ylab ="Tempo de execução (em segundos)")
+plot(testeExames_Prob1_without_outlier$probability_pairs,testeExames_Prob1_without_outlier$time_in_seconds, 
+     main = "Teste Exames e Prob 1 - Sem outliers (Exames 10 a 60, 2 em 2)", 
+     xlab = "Probabilidade",
+     ylab ="Tempo de execução (em segundos)")
+plot(ag_testeExames_Prob1$probability_pairs,ag_testeExames_Prob1$time_in_seconds, 
+     main = "Teste Exames e Prob 1 - Sem outliers, valor médio por número de exames (Exames 10 a 60, 2 em 2)", 
+     xlab = "Probabilidade",
+     ylab ="Tempo de execução (em segundos)")
 
 
 
 
 
 
-plot(data1$probability_pairs,data1$time_in_seconds)
+
+plot(testeProb1$probability_pairs,testeProb1$time_in_seconds)
 plot(data2$probability_pairs,data2$time_in_seconds)
 
 
-lm.out=lm(data1$time_in_seconds~data1$probability_pairs)
+lm.out=lm(testeProb1$time_in_seconds~testeProb1$probability_pairs)
 summar = summary(lm.out)
--- script2.sh
+++ script2.sh
@@ -4,14 +4,14 @@ gcc code1.c -o code1 -O3
 gcc code2.c -o code2 -O3
 
 
-MAX_ITERATIONS=25
+MAX_ITERATIONS=5
 
 FILENAME_DATA_INPUT="data.in"
 
 # Variáveis do gen.py
-N_EXAMS=35  # Max 299 segundo o enunciado
+N_EXAMS=50  # Max 299 segundo o enunciado
 PROBABILITY_PAIRS=20 # Out of 100
-EXAMS_INCREMENT_VALUE=1
+EXAMS_INCREMENT_VALUE=2
 SEED_GEN=$(shuf -i1-30000 -n1) 
 # Variáveis do programa
 SEED_PROGRAM=$(shuf -i1-30000 -n1) 
@@ -28,20 +28,23 @@ while [ $I -le $MAX_ITERATIONS ]
 do
     
 
-    for j in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
+    for j in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
     do
-        # Run code
-        python3 gen.py $N_EXAMS $PROBABILITY_PAIRS $SEED_GEN $FILENAME_DATA_INPUT
-        SEED_GEN=$(shuf -i1-30000 -n1) 
 
-        for k in 1 2
+        for k in 2 5 10 25 1
         do
-            echo -n "$N_EXAMS $PROBABILITY_PAIRS $SEED_GEN $SEED_PROGRAM " >> results_code1.txt
+            # Run code
+            python3 gen.py $N_EXAMS $k $SEED_GEN $FILENAME_DATA_INPUT
+            SEED_GEN=$(shuf -i1-30000 -n1) 
+    
+            echo -n "$N_EXAMS $k $SEED_GEN $SEED_PROGRAM " >> results_code1.txt
             ./code1 $SEED_PROGRAM $MAX_TIME $FILENAME_DATA_INPUT >> results_code1.txt
-            echo -n "$N_EXAMS $PROBABILITY_PAIRS $SEED_GEN $SEED_PROGRAM " >> results_code2.txt
+            echo -n "$N_EXAMS $k $SEED_GEN $SEED_PROGRAM " >> results_code2.txt
             ./code2 $SEED_PROGRAM $MAX_TIME $FILENAME_DATA_INPUT >> results_code2.txt
         done
+
         SEED_PROGRAM=$(shuf -i1-30000 -n1) 
+        
     done
 
     # Increment variables
